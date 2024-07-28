9ae9de0f858b2e2e9a7c1a43cb53420468a832ac
Sabarish8817 <89400552+Sabarish8817@users.noreply.github.com>
2024-07-28 21:35:12 +0530

    Create Trigger.java

---
+++ Trigger.java
@@ -0,0 +1,14 @@
+public class Trigger {
+    private String condition;
+    private String action;
+
+    public Trigger(String condition, String action) {
+        this.condition = condition;
+        this.action = action;
+    }
+
+    @Override
+    public String toString() {
+        return "Trigger: " + condition + " -> " + action;
+    }
+}
